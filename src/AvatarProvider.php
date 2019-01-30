56cf1f693c0ebf89bc86c33ea3c71e1da3b3662d
zhouyao-sxs <zhou.yao@email.shaxiaoseng.com>
2019-01-30 17:58:42 +0800

    init

---
+++ src/AvatarProvider.php
@@ -0,0 +1,29 @@
+<?php
+namespace Cxp\Avatar;
+use Illuminate\Support\ServiceProvider;
+class AvatarProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        // 发布配置文件
+        $this->publishes([
+            __DIR__.'/config/avatar.php' => config_path('avatar.php'),
+        ]);
+    }
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->singleton('avatar', function ($app) {
+            return new Avatar($app['config']);
+        });
+    }
+}
